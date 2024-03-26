4b75c4c0fab56b3f1c3c2ac62e22364b91a47288
Laurent <laurent.roches.simplon@gmail.com>
2024-03-26 15:25:54 +0100

    nom bdd

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
 
     define('DB_HOST', 'localhost');
-    define('DB_NAME', 'Vercors_bdd');
-    define('DB_USER', 'Vercors_bdd');
+    define('DB_NAME', 'vercors_bdd');
+    define('DB_USER', 'vercors_bdd');
     define('DB_PWD', 'Vercorsbdd');
-    define('PREFIXE', 'Vercors_');
+    define('PREFIXE', 'vercors_');
 
 
     define('DB_INITIALIZED', FALSE);
