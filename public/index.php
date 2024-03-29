a794af88ab13e2c763e1fa7fc64148263a3f5e74
Laurent <laurent.roches.simplon@gmail.com>
2024-03-29 14:03:21 +0100

    mep

--- public/index.php
+++ public/index.php
@@ -20,19 +20,19 @@ if (isset($_GET["req"])) {
 
 ?>
 
-    <!DOCTYPE html>
-    <html lang="fr">
+<!DOCTYPE html>
+<html lang="fr">
 
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Vercors festival </title>
-        <link rel="shortcut icon" type="image/png" href="./img/favicon.ico" />
-    </head>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vercors festival </title>
+    <link rel="shortcut icon" type="image/png" href="./img/favicon.ico" />
+</head>
 
-    <body class="relative">
+<body class="relative">
         <!-- Creat Account Modal -->
-        <!-- <div id="creatAccount" class="modal py-24 px-10 top-0 w-screen  min-h-screen bg-gradient-to-b from-teal-400 from-5% via-red-400 via-35%  to-yellow-200 to-95% ">
+<div id="creatAccount" class="modal py-24 px-10 top-0 w-screen  min-h-screen bg-gradient-to-b from-teal-400 from-5% via-red-400 via-35%  to-yellow-200 to-95% ">
             <div class="flex flex-col justify-center px-8 max-w-2xl mx-auto bg-white rounded-3xl">
                 <div class="sm:mx-auto sm:w-full sm:max-w-sm">
                     <h2 class="mt-16 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900 uppercase text-black text-s font-bold">Creat account</h2>
@@ -126,50 +126,50 @@ if (isset($_GET["req"])) {
                 </div>
             </div>
 
-        </div> -->
+</div>
 
 
         <!-- Login modal -->
-        <!-- <div id="loginModal" class="hidden modal flex items-center  px-10 top-0 w-screen h-screen bg-gradient-to-b from-teal-400 from-5% via-red-400 via-35%  to-yellow-200 to-95% ">
+<div id="loginModal" class="hidden modal flex items-center  px-10 top-0 w-screen h-screen bg-gradient-to-b from-teal-400 from-5% via-red-400 via-35%  to-yellow-200 to-95% ">
 
-        <div class="flex flex-col justify-center px-8 max-w-2xl mx-auto bg-white rounded-3xl">
-            <div class="sm:mx-auto sm:w-full sm:max-w-sm ">
-                <h2 class="mt-16 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900 uppercase text-black text-s font-bold">Login</h2>
-            </div>
-            <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+    <div class="flex flex-col justify-center px-8 max-w-2xl mx-auto bg-white rounded-3xl">
+        <div class="sm:mx-auto sm:w-full sm:max-w-sm ">
+            <h2 class="mt-16 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900 uppercase text-black text-s font-bold">Login</h2>
+        </div>
+        <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
 
-                <div class="space-y-6">
+            <div class="space-y-6">
 
-                    <div>
-                        <label for="email" class="block text-sm font-medium leading-6 text-gray-900 uppercase text-black text-s font-semibold">Email address</label>
-                        <div class="mt-2">
-                            <input id="email" name="email" type="email" autocomplete="email" required class="block w-full bg-orange-50 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-orange-300  placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-orange-500 sm:text-sm sm:leading-6 indent-3 emailInput">
-                        </div>
+                <div>
+                    <label for="email" class="block text-sm font-medium leading-6 text-gray-900 uppercase text-black text-s font-semibold">Email address</label>
+                    <div class="mt-2">
+                        <input id="email" name="email" type="email" autocomplete="email" required class="block w-full bg-orange-50 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-orange-300  placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-orange-500 sm:text-sm sm:leading-6 indent-3 emailInput">
                     </div>
+                </div>
 
-                    <div>
-                        <div class="flex items-center justify-between">
-                            <label for="password" class="block text-sm font-medium leading-6 text-gray-900 uppercase text-black text-s font-semibold">Password</label>
-                        </div>
-                        <div class="mt-2">
-                            <input id="password" name="password" type="password" autocomplete="current-password" required class="block w-full bg-orange-50 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-orange-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-orange-500 sm:text-sm sm:leading-6 indent-3 passwordInput mb-10">
-                        </div>
+                <div>
+                    <div class="flex items-center justify-between">
+                        <label for="password" class="block text-sm font-medium leading-6 text-gray-900 uppercase text-black text-s font-semibold">Password</label>
                     </div>
-                    <div id="loginToast" class="hidden">
-                        <p id="loginToastText" class=" toast flex w-full justify-center rounded-md bg-red-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white uppercase text-s font-semibold opacity-80 ">
-                        </p>
+                    <div class="mt-2">
+                        <input id="password" name="password" type="password" autocomplete="current-password" required class="block w-full bg-orange-50 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-orange-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-orange-500 sm:text-sm sm:leading-6 indent-3 passwordInput mb-10">
                     </div>
+                </div>
+                <div id="loginToast" class="hidden">
+                    <p id="loginToastText" class=" toast flex w-full justify-center rounded-md bg-red-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white uppercase text-s font-semibold opacity-80 ">
+                    </p>
+                </div>
 
-                    <div>
-                        <button onclick="loginAccount()" class=" my-10 flex w-full justify-center rounded-md bg-orange-300 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-orange-500 uppercase text-white text-s font-semibold">Login</button>
-                    </div>
-                    <div>
-                        <button onclick="creatModal()" class=" mb-16 flex w-full justify-center rounded-md bg-orange-300 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-orange-500 uppercase text-white text-s font-semibold">Not a member ? Creat an account</button>
-                    </div>
+                <div>
+                    <button onclick="loginAccount()" class=" my-10 flex w-full justify-center rounded-md bg-orange-300 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-orange-500 uppercase text-white text-s font-semibold">Login</button>
+                </div>
+                <div>
+                    <button onclick="creatModal()" class=" mb-16 flex w-full justify-center rounded-md bg-orange-300 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-orange-500 uppercase text-white text-s font-semibold">Not a member ? Creat an account</button>
                 </div>
             </div>
         </div>
-    </div> -->
+    </div>
+</div>
 
 
 </body>
