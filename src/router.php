b8cc39eb69f9823b0911f07656ec5c5ee8f665ef
Tonie <tonie.lanquette.simplon@gmail.com>
2024-04-04 13:41:37 +0200

    début traitement

--- src/router.php
+++ src/router.php
@@ -1,12 +1,14 @@
 <?php
 
 use src\Controllers\HomeController;
+use src\Controllers\ResaController;
 use src\Services\Routing;
 
 $route = $_SERVER['REDIRECT_URL'];
 $methode = $_SERVER['REQUEST_METHOD'];
 
 $HomeController = new HomeController;
+$ResaController= new ResaController;
 
 // ici le :: permet d'appeler la fonction static de la class Routing
 $routeComposee = Routing::routeComposee($route);
@@ -36,5 +38,9 @@ switch($route) {
         break;
     case HOME_URL . 'dashboard':
         $HomeController->pageDashboard();
+        break;
+    case HOME_URL . 'addResa':
+        $ResaController->addResa();
+        header("location:" . HOME_URL."dashboard");
         break;
 }
